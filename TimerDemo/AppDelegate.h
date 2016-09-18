6e96d113d7253bf0fe107c234149c0c8cd7fe5db
吕东 <lvdong@tcrj.com.cn>
2016-09-18 09:38:10 +0800

    Initial Commit

---
+++ TimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimerDemo
+//
+//  Created by apple on 16/9/18.
+//  Copyright © 2016年 吕东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerDemo
+//
+//  Created by apple on 16/9/18.
+//  Copyright © 2016年 吕东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
